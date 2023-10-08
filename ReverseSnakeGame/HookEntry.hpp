d6bc36853a91e57dfcb33e26ca97054264208a59
thinct <thinct_123@foxmail.com>
2023-10-08 21:59:26 +0800

    上一次上传的内容有问题。本次上传的内容才是正确的Demo源码工程。

---
+++ ReverseSnakeGame/HookEntry.hpp
@@ -0,0 +1,61 @@
+﻿#ifndef __HOOKENTRY_HPP__
+#define __HOOKENTRY_HPP__
+
+#include "InjectHooker.hpp"
+
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+#include <string>
+#include <stdexcept>
+#include <deque>
+#include <functional>
+
+#include "SnakeGame.hpp"
+
+
+void Hello()
+{
+    sf::RenderWindow window(sf::VideoMode(300, 300), L"我是标题");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
+
+
+typedef void(__cdecl* FUNCTION_TEST)(sf::Time&);
+HOOK_TARGET(FUNCTION_TEST, SnakeGame__Update, 0x00407A60);
+
+#define GEN_HOOKER GEN_HOOKER_FUNC(SnakeGame__Update);
+#define UNGEN_HOOKER UNGEN_HOOKER_FUNC(SnakeGame__Update);
+
+SnakeGame* pSnakeGame = nullptr;
+
+void __cdecl DETOURFUNCION(SnakeGame__Update)(const sf::Time& deltaTime)
+{
+    //EXPAND_NOP_ASM_X10(1);
+
+    __asm {
+        mov pSnakeGame, ecx
+
+        mov         eax, dword ptr[deltaTime]
+        push        eax
+        mov         edx, dword ptr[ecx]
+        mov         eax, SnakeGame::update
+        call        eax
+    }
+}
+
+#endif // __HOOKENTRY_HPP__
---
+++ ReverseSnakeGame/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <SFML/Graphics.hpp>
+
+#include <Windows.h>
+#include <string>
+#include <stdexcept>
+#include <deque>
+
+#include "InjectHooker.hpp"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR pCmdLine, _In_ int nCmdShow)
+{
+    // Hello();
+	return 0;
+}
